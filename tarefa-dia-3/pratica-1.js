db54496339c62702a93fc29317096dc8b6ed3ae0
Anderson Luz <andersonluz87@gmail.com>
2022-06-24 18:34:17 -0300

    Realizado o item 13 do exercício

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -4,8 +4,7 @@ let profissao = "Técnico eletrotécnico"
 let idade = 35
 let lugarDosSonhos = "Europa"
 
-console.log(nome, 
-     email, 
-     profissao, 
-     idade, 
-     lugarDosSonhos)
+//console.log(nome,email,profissao,idade,lugarDosSonhos)
+
+     console.log("Olá,",nome ,"Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ",lugarDosSonhos,". Confirma pra gente alguns dados? A sua idade é ",idade,", você é ",profissao," e o seu email é ",email,"? Ficamos no aguardo. Muito obrigado e boa viagem! :D")
+
