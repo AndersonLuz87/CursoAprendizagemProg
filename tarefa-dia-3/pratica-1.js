a91a23a86500d6c94d84176761bbdfdc3070fc5d
Anderson Luz <andersonluz87@gmail.com>
2022-06-24 17:45:26 -0300

    Criando comando console.log das variaveis criadas

--- tarefa-dia-3/pratica-1.js
+++ tarefa-dia-3/pratica-1.js
@@ -3,3 +3,9 @@ let email = "andersonluz87@gmail.com"
 let profissao = "Técnico eletrotécnico"
 let idade = 35
 let lugarDosSonhos = "Europa"
+
+console.log(nome, 
+     email, 
+     profissao, 
+     idade, 
+     lugarDosSonhos)
