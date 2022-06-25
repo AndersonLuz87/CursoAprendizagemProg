b7cfdbded8ca587a54130d73738d6f1743500180
Anderson Luz <andersonluz87@gmail.com>
2022-06-25 00:59:18 -0300

    Passo 2 da tarefa do dia 4

---
+++ tarefa-dia-4/pratica-2.js
@@ -0,0 +1,11 @@
+console.log("")
+console.log("TABUADA DO 7")
+console.log("")
+for (let contador = 7; contador <=70; contador +=7) {
+    console.log("7 x",contador/7,"=",contador)
+}
+    // escreva aqui um código para calcular exibir o resultado da tabuada do número 7. Ou seja, exibir o resultado de quanto é 7x1, 7x2, 7x3, 7x4, 7x5, etc
+    // veja que você pode utilizar o contador para fazer este cálculo.
+
+
+
